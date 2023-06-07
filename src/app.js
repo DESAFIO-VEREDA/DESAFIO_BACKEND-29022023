1dddea522d5ccb8858d98f1372e5e6158ad5a723
Thiago Della Coletta Azevedo <cmsthiagoazevedo@gmail.com>
2023-06-07 17:22:10 -0300

    Queria muito mostrar que não desisto

--- src/app.js
+++ src/app.js
@@ -8,21 +8,20 @@
 
   app.get('/', (req, res) => {
     const {from, to, ammount} = req.query;
-    //com essa construção no axios consigo buscar dinamicamente a cotação entre moedas
     axios.get(`https://economia.awesomeapi.com.br/json/last/${from}-${to}`,{
       params: {
         from: from,
         to: to,        
       }
-    })
-    .then(function (response) {
+    }).then(function (response) {
       const {data} = response;
-      //a variavel inData deveria pegar os dados From e To para criar a chave
-      //do objeto dentro de data onde chegaria no valor da moeda para então
-      //fazer o calculo da cotação
-      const inData = `${from}`+`${to}`      
-      console.log(data)
-    })
-  } )
+      const dynamicKey = Object.keys(data)[0];  
+      const cotacao = data[dynamicKey].high;
+      const conversao = (Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(cotacao * ammount))
+      res.status(200).json(conversao)
+    }).catch((err) => {      
+      res.status(403).json({message: "Cryptos não podem ser valores destino"})
+    })      
+    })  
 
   module.exports = app;
