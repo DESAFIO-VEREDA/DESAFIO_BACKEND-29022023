cf9719e96d9dcab5e9215d9ee5382b8127b0c3e0
Thiago Della Coletta Azevedo <cmsthiagoazevedo@gmail.com>
2023-06-07 12:22:06 -0300

    Explicando minha lógica no código

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@
 
   app.get('/', (req, res) => {
     const {from, to, ammount} = req.query;
+    //com essa construção no axios consigo buscar dinamicamente a cotação entre moedas
     axios.get(`https://economia.awesomeapi.com.br/json/last/${from}-${to}`,{
       params: {
         from: from,
@@ -16,6 +17,9 @@
     })
     .then(function (response) {
       const {data} = response;
+      //a variavel inData deveria pegar os dados From e To para criar a chave
+      //do objeto dentro de data onde chegaria no valor da moeda para então
+      //fazer o calculo da cotação
       const inData = `${from}`+`${to}`      
       console.log(data)
     })
